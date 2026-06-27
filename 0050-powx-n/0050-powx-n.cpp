221801d32462d6a28d976f064a137b295a059e6c
pixel-library <rudrasharma6273@gmail.com>
2026-06-27 12:44:24 +0530

    Time: 0 ms (100%), Space: 8.8 MB (11.87%) - LeetHub - Time: O(log(n)), Space: O(1)

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        if(n == 0) return 1.0;
+        if(x == 0) return 0.0;
+        if(x == 1) return 1.0;
+        if( x == -1 && n%2 == 0) return 1.0;
+        if( x == -1 && n%2 != 0) return -1.0;
         
         long binform = n;
         if(n < 0) {
